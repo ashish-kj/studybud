c98817fd29f6a979e68966aa5093f2c584eb5ad5
Ashish-BITS <f20180173@dubai.bits-pilani.ac.in>
2022-07-30 16:11:17 +0400

    2:30:59

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/',views.loginPage, name="login"),
     path('',views.home,name="home"),
     path('room/<str:pk>/',views.room,name="room"),
     path('create-room/',views.createRoom,name="create-room"),
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from pydoc_data.topics import topics
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -11,13 +12,22 @@ from .forms import RoomForm
 #     {'id':3,'name':'Frontend developers'},
 # ]
 
+def loginPage(request):
+    context = {}
+    return render(request,'base/login_register.html', context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q')!=None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
 
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
-    context = {'rooms':rooms, 'topics':topics}
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request,'base/home.html',context)
 
 def room(request, pk):
