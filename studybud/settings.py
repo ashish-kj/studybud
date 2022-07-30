d263edcc7a99fc591e453f43bc8671a37f3393e9
Ashish-BITS <f20180173@dubai.bits-pilani.ac.in>
2022-07-30 22:48:43 +0400

    4:04:10

--- studybud/settings.py
+++ studybud/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
+#STATIC_ROOT = 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
