c993ed0dbed5f00889e1359e1904214e74eea3f2
Ashish-BITS <f20180173@dubai.bits-pilani.ac.in>
2022-07-31 22:23:56 +0400

    5:53:18

--- base/views.py
+++ base/views.py
@@ -186,3 +186,7 @@ def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q')!=None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html',{'topics':topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html',{'room_messages':room_messages})
