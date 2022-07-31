7cb7a0be9b263c8e83b78ad44452b2d009093c4a
Ashish-BITS <f20180173@dubai.bits-pilani.ac.in>
2022-07-31 22:17:02 +0400

    5:47:54

--- base/urls.py
+++ base/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('delete-room/<str:pk>',views.deleteRoom,name="delete-room"),
     path('delete-message/<str:pk>',views.deleteMessage,name="delete-message"),
     path('update-user/',views.updateUser,name="update-user"),
+    path('topics/',views.topicsPage,name="topics"),
 ]
--- base/views.py
+++ base/views.py
@@ -71,7 +71,7 @@ def home(request):
         Q(description__icontains=q)
         )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -180,3 +180,9 @@ def updateUser(request):
     
     return render(request,'base/update-user.html',{'form':form})
 
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q')!=None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html',{'topics':topics})
